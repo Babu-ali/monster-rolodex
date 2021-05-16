74543cb24022b5f38035166daf608a2496ee4bbd
babua <babua@maveric-systems.com>
2021-05-17 00:44:55 +0530

    added monster images from local

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './card.styles.css';
+import robo from './newMonster.png';
+
+
 
 export const Card = props => (
  <div className='card-container'>
      <img
      alt="monster"
-     src="./robo.png"
-     width="180"
+     //src={'https://robohash.org/3?set=set2'}
+     src={robo}
+     width="250"
      height="180"
      />
      <h2>{props.monster.name}</h2>
